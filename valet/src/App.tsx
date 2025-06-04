fd5a2f57482d538c003ae0df2df056cb35c1e47d
Nitin Sharma <nitin@fuelbuddy.in>
2025-06-04 13:02:43 +0000

    feat: add shad/cn

--- valet/src/App.tsx
+++ valet/src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { FrappeProvider } from 'frappe-react-sdk'
+import { Button } from "@/components/ui/button"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 	<div className="App">
 	  <FrappeProvider>
+		<Button>Click me</Button>
 		<div>
 	  <div>
 		<a href="https://vitejs.dev" target="_blank">
